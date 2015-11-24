9fc596f9b8fe33aaf54b5780624a1c85b3efdabc
SoraZodia <SoraZodia@users.noreply.github.com>
2015-11-23 23:25:14 -1000

    Wendigo got a bit scary

--- src/main/java/sorazodia/cannibalism/mechanic/events/EntityNBTEvents.java
+++ src/main/java/sorazodia/cannibalism/mechanic/events/EntityNBTEvents.java
@@ -12,7 +12,6 @@ import sorazodia.cannibalism.main.Cannibalism;
 import sorazodia.cannibalism.mechanic.nbt.CannibalismNBT;
 import sorazodia.cannibalism.mob.EntityWendigo;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import cpw.mods.fml.common.gameevent.TickEvent.PlayerTickEvent;
 
 public class EntityNBTEvents
 {
@@ -25,53 +24,59 @@ public class EntityNBTEvents
 		if (ConfigHandler.getMyth() == false)
 			return;
 
-		if ((create.entity instanceof EntityPlayer)
-				&& CannibalismNBT.getNBT((EntityLivingBase) create.entity) == null)
+		if ((create.entity instanceof EntityPlayer) && CannibalismNBT.getNBT((EntityLivingBase) create.entity) == null)
 		{
 			CannibalismNBT.register((EntityLivingBase) create.entity);
 		}
 	}
-	
+
 	@SubscribeEvent
 	public void playerUpdate(LivingUpdateEvent updateEvent)
 	{
-		if (updateEvent.entityLiving instanceof EntityPlayer && ConfigHandler.getMyth() && CannibalismNBT.getNBT(updateEvent.entityLiving) != null)
+		if (!updateEvent.entityLiving.worldObj.isRemote && updateEvent.entityLiving instanceof EntityPlayer && ConfigHandler.getMyth() && CannibalismNBT.getNBT(updateEvent.entityLiving) != null)
 		{
 			EntityPlayer player = (EntityPlayer) updateEvent.entityLiving;
-			float sinLevel = CannibalismNBT.getNBT(player).getWendigoValue();
+			float wendigoLevel = CannibalismNBT.getNBT(player).getWendigoValue();
 			
-			if (sinLevel >= 50)
-			{
-				player.addPotionEffect(new PotionEffect(Potion.moveSpeed.id, 10));
-				player.addExhaustion(0.02F);
-			}
+			addWendigoAbility(player, wendigoLevel);
+			wendigoSpawn(player, wendigoLevel);
+		}
+	}
+	
+	private void addWendigoAbility(EntityPlayer player, float wendigoLevel)
+	{
+		if (wendigoLevel >= 50 && wendigoLevel < 100)
+		{
+			player.addPotionEffect(new PotionEffect(Potion.moveSpeed.id, 10));
+			player.addExhaustion(0.02F);
+		}
+		if (wendigoLevel >= 100)
+		{
+			player.addPotionEffect(new PotionEffect(Potion.moveSpeed.id, 10, 1));
+			player.addExhaustion(0.04F);
+		}
+		if (wendigoLevel >= 150)
+		{
+			player.addPotionEffect(new PotionEffect(Potion.damageBoost.id, 10, 2));
+			player.addExhaustion(0.08F);
 		}
 	}
 
-	@SubscribeEvent
-	public void entityTick(PlayerTickEvent tickEvent)
+	private void wendigoSpawn(EntityPlayer player, float wendigoLevel)
 	{
-		EntityPlayer player = tickEvent.player;
-		
-		if (ConfigHandler.getMyth() == false || CannibalismNBT.getNBT(player) == null)
-			return;
-
-		float sinLevel = CannibalismNBT.getNBT(player).getWendigoValue();
-
-		if (sinLevel >= 50 && applyEffect)
+		if (wendigoLevel >= 50 && applyEffect)
 		{
 			player.addPotionEffect(new PotionEffect(Potion.blindness.id, 100));
 			player.addPotionEffect(new PotionEffect(Potion.confusion.id, 100));
 			applyEffect = false;
 		}
 
-		if (sinLevel >= 250)
+		if (wendigoLevel >= 250)
 		{
 
 			if (CannibalismNBT.getNBT(player).wendigoSpawned() == false)
 			{
-				EntityWendigo wendigo = (EntityWendigo) EntityList.createEntityByName(Cannibalism.MODID
-						+ ".wendigo", player.worldObj);
+				EntityWendigo wendigo = (EntityWendigo) EntityList.createEntityByName(Cannibalism.MODID + ".wendigo", player.worldObj);
 				wendigo.setLocationAndAngles(player.posX - 40, player.posY, player.posZ, 0, 0);
 				player.worldObj.spawnEntityInWorld(wendigo);
 
--- src/main/java/sorazodia/cannibalism/mob/EntityWendigo.java
+++ src/main/java/sorazodia/cannibalism/mob/EntityWendigo.java
@@ -7,6 +7,7 @@ import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.DamageSource;
 import net.minecraft.world.World;
+import sorazodia.cannibalism.main.Cannibalism;
 import sorazodia.cannibalism.mechanic.nbt.CannibalismNBT;
 
 public class EntityWendigo extends EntityMob
@@ -15,8 +16,7 @@ public class EntityWendigo extends EntityMob
 	public EntityWendigo(World world)
 	{
 		super(world);
-		//this.yOffset *= 8.5F;
-		//this.setSize(width * 4F, height * 8.5F);
+		this.setSize(width, height * 2.0F);
 	}
 
 	@Override
@@ -24,10 +24,9 @@ public class EntityWendigo extends EntityMob
 	{
 		super.applyEntityAttributes();
 		getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(1D);
-		getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(0D);
 		getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(100D);
-		getEntityAttribute(SharedMonsterAttributes.followRange).setBaseValue(40D);
-		getEntityAttribute(SharedMonsterAttributes.knockbackResistance).setBaseValue(1000D);
+		getEntityAttribute(SharedMonsterAttributes.followRange).setBaseValue(42D);
+		getEntityAttribute(SharedMonsterAttributes.knockbackResistance).setBaseValue(100D);
 	}
 
 	@Override
@@ -42,34 +41,66 @@ public class EntityWendigo extends EntityMob
 				if (CannibalismNBT.getNBT(player) != null)
 					CannibalismNBT.getNBT(player).setWedigoSpawn(false);
 			}
+		} else
+		{
+			this.stepHeight = 1.0F;
 		}
 	}
 
 	@Override
 	public boolean attackEntityAsMob(Entity entity)
 	{
-		boolean attacked = super.attackEntityAsMob(entity);
-		if (attacked && entity instanceof EntityLivingBase)
+		boolean attacked = false;
+		if (entity instanceof EntityLivingBase)
 		{
 			EntityLivingBase target = (EntityLivingBase) entity;
-			
-			target.motionX += 2.0;
-			target.motionY += 1.0;
-			target.motionZ += 2.0;
-			
-			target.setHealth(target.getHealth() - 10);
-			if (target.getHealth() < 0.01F)
+
+			attacked = target.attackEntityFrom(DamageSource.causeMobDamage(this), 1.0F);
+			if (attacked)
 			{
-				target.onDeath(DamageSource.causeMobDamage(this));
+				target.setHealth(target.getHealth() - 10);
+				target.motionY *= 2;
+				target.motionX *= 2;
+				target.motionZ *= 2;
+				
+				if (target.getHealth() <= 0.01F)
+					target.onDeath(DamageSource.causeMobDamage(this).setDamageBypassesArmor());
 			}
+
+			
 		}
 
 		return attacked;
 	}
 
+	@Override
+	public boolean canDespawn()
+	{
+		return false;
+	}
+
+	@Override
 	public int getExperiencePoints(EntityPlayer player)
 	{
 		return 15;
+	}
+
+	@Override
+	public String getLivingSound()
+	{
+		return Cannibalism.MODID + ":mob.wendigo.grr";
+	}
+
+	@Override
+	public String getHurtSound()
+	{
+		return Cannibalism.MODID + ":mob.wendigo.hurt";
+	}
+
+	@Override
+	public String getDeathSound()
+	{
+		return "mob.ghast.death";
 	}
 
 }
