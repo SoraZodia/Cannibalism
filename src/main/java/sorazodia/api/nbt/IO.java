cb36f74d90062b2b85c24e4e58337ea94366b7a7
SoraZodia <SoraZodia@users.noreply.github.com>
2017-01-21 23:33:07 -1000

    Overloaded read() and write()
    These function will have the path already set

--- src/main/java/sorazodia/api/nbt/IO.java
+++ src/main/java/sorazodia/api/nbt/IO.java
@@ -9,27 +9,68 @@ import java.io.ObjectOutputStream;
 import java.util.HashMap;
 import java.util.UUID;
 
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+
 /**
  * Read and write the Database object to file
  */
 public class IO
 {
+	
+	private String dirPath= "";
+	private String filePath = "";
+	
 	/**
-	 * Convert the Database into an external file for later use
+	 * Creates a IO object that has a preset path for the data file, so that
+	 * the non-static variation of {@link #read(String)} and {@link #write(String, HashMap)}
+	 * can be used.
 	 * 
-	 * @param File path to write the file to
-	 * @param The HashMap stored in the Database object
-	 * @throws IOException
+	 * @param FMLServerStartingEvent
+	 * @param file name, please use your modID to avoid conflicts. The resulting file will be a .dat file.
 	 */
-	public static void write(String path, HashMap<UUID, PlayerInfo> data) throws IOException
+	public IO(FMLServerStartingEvent event, String fileName)
 	{
-		File file = new File(path);
-		ObjectOutputStream writer = new ObjectOutputStream(new FileOutputStream(file));
-
-		writer.writeObject(data);
-		writer.close();
+		this.dirPath = ".\\saves\\" + event.getServer().getFolderName() + "\\SavedPlayerData";
+		this.filePath = dirPath + "\\" + fileName + ".dat";
 	}
 
+	/**
+	 * Convert a Database file into a object to be used
+	 */
+	@SuppressWarnings("unchecked")
+	public Database read()
+	{
+		
+		File dir = new File(dirPath);
+		File file = new File(filePath);
+		Database database = new Database();
+
+		if (!dir.exists())
+		{
+			dir.mkdirs();
+		}
+		
+		try
+		{
+			if (file.exists())
+			{
+				ObjectInputStream reader = new ObjectInputStream(new FileInputStream(file));
+
+				HashMap<UUID, PlayerInfo> data = (HashMap<UUID, PlayerInfo>) reader.readObject();
+				reader.close();
+
+				database = new Database(data);
+			}
+		}
+		catch (ClassNotFoundException | IOException e)
+		{
+			e.printStackTrace();
+		}
+		
+		return database;
+		
+	}
+	
 	/**
 	 * Convert a Database file into a object to be used
 	 * 
@@ -60,4 +101,41 @@ public class IO
 
 		return database;
 	}
+	
+	/**
+	 * Convert the Database into an external file for later use
+	 * 
+	 * @param The HashMap stored in the Database object
+	 */
+	public void write(HashMap<UUID, PlayerInfo> data)
+	{
+		ObjectOutputStream writer;
+		try
+		{
+			writer = new ObjectOutputStream(new FileOutputStream(filePath));
+			writer.writeObject(data);
+			writer.close();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Convert the Database into an external file for later use
+	 * 
+	 * @param File path to write the file to
+	 * @param The HashMap stored in the Database object
+	 * @throws IOException
+	 */
+	public static void write(String path, HashMap<UUID, PlayerInfo> data) throws IOException
+	{
+		File file = new File(path);
+		ObjectOutputStream writer = new ObjectOutputStream(new FileOutputStream(file));
+
+		writer.writeObject(data);
+		writer.close();
+	}
+
 }
