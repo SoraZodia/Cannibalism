f42a289067581a05800a60434145fd2b0cbee76c
SoraZodia <sorazodia@gmail.com>
2019-05-04 23:26:10 -1000

    bump version

--- src/main/java/sorazodia/cannibalism/main/Cannibalism.java
+++ src/main/java/sorazodia/cannibalism/main/Cannibalism.java
@@ -38,7 +38,7 @@ public class Cannibalism
 {
 
 	public static final String MODID = "cannibalism";
-	public static final String VERSION = "5.3.0";
+	public static final String VERSION = "5.4.0";
 	public static final String NAME = "Cannibalism";
 	public static final String GUI_FACTORY = "sorazodia.cannibalism.config.ConfigGUIFactory";
 
