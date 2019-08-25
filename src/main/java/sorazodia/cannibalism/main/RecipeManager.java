395576aa6481ff6e455ef00b913b367332c445e8
SoraZodia <sorazodia@gmail.com>
2019-08-25 12:53:23 -1000

    Fix item not found error when knife is disabled

---
+++ src/main/java/sorazodia/cannibalism/main/RecipeManager.java
@@ -0,0 +1,27 @@
+package sorazodia.cannibalism.main;
+
+import java.util.function.BooleanSupplier;
+
+import com.google.gson.JsonObject;
+
+import net.minecraftforge.common.crafting.IConditionFactory;
+import net.minecraftforge.common.crafting.JsonContext;
+import sorazodia.cannibalism.config.ConfigHandler;
+import sorazodia.cannibalism.items.KnifeType;
+
+public class RecipeManager implements IConditionFactory
+{
+	private final String ID_KEY = "id";
+	private final String MOD_KEY = "modid";
+	
+	@Override
+	public BooleanSupplier parse(JsonContext context, JsonObject json)
+	{	
+		if ((json.has(ID_KEY) && json.has(MOD_KEY)) && json.get(MOD_KEY).getAsString().equals(Cannibalism.MODID)) {
+			return () -> ConfigHandler.getEnabledKnifeList().contains(KnifeType.valueOf(json.get(ID_KEY).getAsString().toUpperCase()));
+		}
+		
+		return () -> true;
+	}
+
+}
